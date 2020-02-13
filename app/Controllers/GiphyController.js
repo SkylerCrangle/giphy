191b22aa9209c213b38ad6e7a41ff05642141e77
SkylerCrangle <smcrangle@gmail.com>
2020-02-13 16:08:50 -0700

    search and randomize working

--- app/Controllers/GiphyController.js
+++ app/Controllers/GiphyController.js
@@ -1,12 +1,16 @@
 import GiphyService from "../Services/GiphyService.js";
 import store from "../store.js";
+import Gifs from "../Models/Gifs.js";
 
 //Private
 function _draw() {
-  let gifs = store.State.dataGifs['Template']
+  let template = "";
+  store.State.dataGifs.forEach(item => {
+    template += item.Template;
+  })
 
-  document.getElementById("gif-place").innerHTML = gifs
-  console.log(gifs);
+  document.getElementById("gif-place").innerHTML = template
+  // console.log(gifs);
 }
 
 //Public
@@ -21,9 +25,9 @@ export default class GiphyController {
 
   getGiphyStuff(event) {
     event.preventDefault();
-    let formData = event.target;
-    GiphyService.getGifsByQuery(formData.query.value);
-    formData.reset();
+    let searchTerm = event.target.search.value;
+    GiphyService.getGifsByQuery(searchTerm);
+    event.target.reset();
 
   }
 
--- app/Services/GiphyService.js
+++ app/Services/GiphyService.js
@@ -6,7 +6,9 @@ let _sandbox = axios.create({
   timeout: 3000,
 });
 let _api = axios.create({
-  baseURL: "//api.giphy.com/v1/gifs/random?api_key=0HXQDgonECHtcd2pUYA7an0niiyVq4az",
+  baseURL: "//api.giphy.com/v1/gifs",
+
+  //random?api_key=0HXQDgonECHtcd2pUYA7an0niiyVq4az",
   timeout: 3000,
 });
 
@@ -20,9 +22,9 @@ class GiphyService {
   }
 
   getRandom() {
-    _api.get("").then(res => {
+    _api.get("random?api_key=0HXQDgonECHtcd2pUYA7an0niiyVq4az").then(res => {
       let gif = new Gifs(res.data.data)
-      store.commit("dataGifs", gif)
+      store.commit("dataGifs", [gif])
       // console.log(res)
     })
   }
@@ -33,15 +35,14 @@ class GiphyService {
   /** @param { string } query
      */
   getGifsByQuery(query) {
+    // debugger;
     //NOTE You will not need to change this method
-    let url = "http://api.giphy.com/v1/gifs/search?api_key=APIKEY&q=" + query;
+    let url = "search?api_key=0HXQDgonECHtcd2pUYA7an0niiyVq4az&q=" + query;
     // @ts-ignore
-    $.getJSON(url)
+    _api.get(url)
       .then(res => {
-        let results = res.results
-          .filter(s => s.kind == "song")
-          .map(rawData => new Gifs(rawData));
-        store.commit("gifs", results);
+        let results = res.data.data.map(rawData => new Gifs(rawData));
+        store.commit("dataGifs", results);
         console.log(store.State.dataGifs)
       })
       .catch(err => {
